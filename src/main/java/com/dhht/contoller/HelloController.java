7fc077cae4fbc8ef789531f593743192955ba29d
wky <948353760@qq.com>
2018-06-24 14:11:25 +0800

    这是master分支

--- src/main/java/com/dhht/contoller/HelloController.java
+++ src/main/java/com/dhht/contoller/HelloController.java
@@ -1,5 +1,6 @@
 package com.dhht.contoller;
-//这是branch1分支制
+//这是branch1分支
+//这是master分支制
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
